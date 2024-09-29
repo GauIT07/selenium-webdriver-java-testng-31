52bb6c980fd64ffdd5ef23654cb5c0640f5a2f6d
GauIT07 <58685733+GauIT07@users.noreply.github.com>
2024-09-30 00:01:24 +0700

    update aumation_31 Topic_04_Selenium_Locator_Baitap

--- selenium/webdriver_31/Topic_04_Selenium_Locator_Baitap.java
+++ selenium/webdriver_31/Topic_04_Selenium_Locator_Baitap.java
@@ -1,10 +1,12 @@
-package webdriver;
+package webdriver_31;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
